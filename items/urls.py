381e92e39ac1faf82d4715122c9b7632c6c9fd8f
Vzhukov642 <vzukov642@gmail.com>
2020-08-26 02:44:19 +0300

    Added robot.txt and sitemap

--- items/urls.py
+++ items/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.conf.urls import url
+from django.http import HttpResponse
 
 from items import views
 
 urlpatterns = [
     path('', views.index, name='item_view'),
-    path('search', views.search, name='search_item')
+    path('search', views.search, name='search_item'),
+    url(r'^robots.txt', lambda x: HttpResponse("User-Agent: *\n"
+                                               "Disallow: /*?\n"
+                                               "Disallow: /admin/\n"
+                                               "Sitemap: static/items/data/sitemap.xml", content_type="text/plain"), name="robots_file"),
 ]
