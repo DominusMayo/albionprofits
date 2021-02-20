a1d95ec8e62b1ca8afe808e2f9b29851cc7a7945
Vzhukov642 <vzukov642@gmail.com>
2021-02-20 18:15:01 +0300

    Change domain in allowed host

--- albiondiff/settings.py
+++ albiondiff/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['albionprofits.tk']
+ALLOWED_HOSTS = ['albionprofits.ru']
 
 
 # Application definition
